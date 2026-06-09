aee1ece411b23df6aafb8fdcb16c3302dacf319e
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-09 20:13:08 +0530

    footer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
-//     smooth: true
-// });
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
 
 // var a = document.querySelector("#elem1");
 // var image = a.getAttribute("data-image")
