9ae463694e58e299c82a42ffc41405e665de2075
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-10 15:24:02 +0530

    completed

--- script.js
+++ script.js
@@ -92,26 +92,29 @@ function menu() {
         if (flag === 0) {
             full.style.top = "0"
             navimg.style.opacity = "0"
-            menuIcon.className = "ri-close-line"  
+            menuIcon.className = "ri-close-line"
             menuText.textContent = "CLOSE"
             flag = 1
         } else {
             full.style.top = "-100%"
             navimg.style.opacity = "1"
-            menuIcon.className = "ri-menu-line"  
+            menuIcon.className = "ri-menu-line"
             menuText.textContent = "MENU"
             flag = 0
         }
     })
 }
 
+function loader() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4000)
+}
+
 elem()
 text()
 swiper()
 menu()
+loader()
 
-
-var loader = document.querySelector("#loader")
-setTimeout(function () {
-    loader.style.top = "-100%"
-}, 4000)
