10c63611160898a6f44cce8606d4849e152e8c8e
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-10 13:36:09 +0530

    responsive

--- script.js
+++ script.js
@@ -57,9 +57,27 @@ function text() {
 function swiper() {
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: "auto",
-        spaceBetween:50,
+        spaceBetween: 50,
     });
 }
+
+var menu = document.querySelector("nav h3")
+var full = document.querySelector("#full-scr")
+var navimg = document.querySelector("nav img")
+var flag = 0
+menu.addEventListener("click", function () {
+    if (flag === 0) {
+        full.style.top = 0
+        navimg.style.opacity = 0
+        flag = 1
+    }
+    else {
+        full.style.top = "-100%"
+        navimg.style.opacity = 1
+        flag = 0
+    }
+
+})
 
 elem()
 text()
